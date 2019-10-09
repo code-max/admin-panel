ab1bf62604a2e5a1da931e363e44348227de7e9c
code-max <code-max@protonmail.com>
2019-10-09 15:26:00 -0300

    Changes in server

--- config/server.js
+++ config/server.js
@@ -8,26 +8,39 @@ const helmet = require('helmet');
 
 const app = express();
 
+// Middlewares
 app.use(cors());
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
   res.send('API');
 });
 
+// Connect Server
 app.listen(process.env.PORT, () =>
   console.log(
     `Server running at http://${process.env.HOSTNAME}:${process.env.PORT}/api`,
   ),
 );
 
-mongoose.connect(`${process.env.DB_URI}`, { useNewUrlParser: true }).then(
-  () => {
-    console.log('MongoDB is connected');
-  },
-  err => {
-    console.log('Can not connect to the database ' + err);
-  },
-);
+// Connect DB
+mongoose
+  .connect(`${process.env.DB_URI}`, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+  .then(
+    () => {
+      console.log('MongoDB is connected');
+    },
+    err => {
+      console.log('Can not connect to the database ' + err);
+    },
+  );
+
+// Routes
+app.use('/api/users', require('../routes/api/users'));
+app.use('/api/auth', require('../routes/api/auth'));
+app.use('/api/profile', require('../routes/api/profile'));
