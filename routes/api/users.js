cc75eb8b384acc687af2d600aaecc16383ba79fb
code-max <code-max@protonmail.com>
2019-10-09 15:25:45 -0300

    Change in api files

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,4 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-router.get('/', (req, res) => res.send('User route'));
+const User = require('../../models/User');
+// @route POST api/users
+// @desc  Register user
+// @access public
+
+router.get(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid mail').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6  o more characters',
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    // console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // See if user exists
+
+    // Encrypt password
+
+    // Return JWT
+
+    res.send('User route');
+  },
+);
+module.exports = router;
